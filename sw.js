02a23cee18301fe1fcddbca8ae832c1032e2e7cf
yyuuyyuu22-sudo <yyuuyyuu22@gmail.com>
2026-02-15 03:27:08 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'sigma-v1.7';
+const ASSETS = ['./index.html', './favicon.ico'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
